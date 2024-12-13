1025786c19df1af2a4eccb8fbc4c79a07ada3016
Saiharshith17 <142712608+Saiharshith17@users.noreply.github.com>
2024-12-13 22:36:30 +0530

    man

--- app/src/main/java/springboot/classes/Man.java
+++ app/src/main/java/springboot/classes/Man.java
@@ -1,6 +1,10 @@
 package springboot.classes;
 
 public class Man implements Person{
-    
-    
+
+    package springboot.classes;
+
+public class Man implements Person{
+    // Add a constructor to initialize the Man object
+   
 }
