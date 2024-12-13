ee27df48d8b522cd3b8c602783a640b2b0eee5b6
Saiharshith17 <142712608+Saiharshith17@users.noreply.github.com>
2024-12-13 22:40:16 +0530

    second commit

--- app/src/main/java/springboot/classes/Man.java
+++ app/src/main/java/springboot/classes/Man.java
@@ -2,9 +2,6 @@ package springboot.classes;
 
 public class Man implements Person{
 
-    package springboot.classes;
+  
 
-public class Man implements Person{
-    // Add a constructor to initialize the Man object
-   
 }
